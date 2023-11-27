9aca41a1771067d96874dede32525c85d2c901a9
2223 Carlos Arrebola <carrebola2223@gmail.com>
2023-11-27 18:11:17 +0100

    .

---
+++ m9_sesion4_sass/src/main.js
@@ -0,0 +1,18 @@
+
+
+
+// importamos el css
+import './scss/style.scss'
+
+document.querySelector('#app').innerHTML = //html
+`
+<h1 class="card">Esto es un título en rojo molón</h1>
+<p class="parrafo">Hola SASS</p>
+<button class="boton">Enviar normal</button>
+<button class="boton-rosa">Enviar rosa</button>
+<p class="texto">Texto doblado</p>
+
+
+
+`
+
