4a1d1adcee9925463fe4f98b0b92b49dd721e7a8
2223 Carlos Arrebola <carrebola2223@gmail.com>
2024-02-05 18:53:16 +0100

    sesión react

---
+++ m9_sesion5_reactTailwin/src/bd.js
@@ -0,0 +1,11 @@
+export const bd = [
+  {
+    nombre: 'gato',
+    imagen: 'https://fotografias-compromiso.atresmedia.com/clipping/cmsimages02/2016/02/26/5659A6D3-EDA9-4B90-BB58-E124E432F1A6/58.jpg'
+  },
+  {
+    nombre: 'perro',
+    imagen: 'https://img.europapress.es/fotoweb/fotonoticia_20170622121827-17062170049_640.jpg'
+  },
+
+]
